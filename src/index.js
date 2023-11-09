359e5ce799c057af01992b045f96abbe14522117
vugar-gasimov <vuqar5qasimov@gmail.com>
2023-11-09 21:57:01 +0200

    install

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <React.StrictMode basename="/goit-react-hw-08-phonebook">
     <App />
   </React.StrictMode>
 );
